a032f6a7be162230a857c1b11b11097b5d677343
VanashriKakad <kakadvanashri@gmail.com>
2023-12-30 18:00:49 +0530

    Auto-commit changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
